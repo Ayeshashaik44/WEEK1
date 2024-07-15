6bf4f76862bcc4ec2949854ff319ed42a375894d
Ayeshashaik44 <skayesha422@gmail.com>
2024-07-15 19:22:44 +0530

    Update graph.py

--- graph.py
+++ graph.py
@@ -1,1 +1,9 @@
+graph={}
+n=int(input("enter the no.of nodes: "))
+for i in range(n):
+    ver=input("enter the vertice:")
+    neigh=input("enter the neighboring nodes:").split()
+    graph[ver]=neigh
+startnode=input("enter the startnode :")
+print(graph[startnode])
 
